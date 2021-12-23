f2e809baabc6917c427e98261f1d2af83b1d2a26
JongMin22 <bbgs0605@naver.com>
2021-12-23 15:45:09 +0900

    2021-12-23

---
+++ 2021-12-23/src/string1/String01P399.java
@@ -0,0 +1,11 @@
+package string1;
+
+public class String01P399 {
+public static void main(String[] args) {
+	// String 클래스의 주요 메서드 확인 1. chartAt();
+	String greeting = "Hello Java";
+	char get = greeting.charAt(6);
+	System.out.println(get);
+	
+}
+}
